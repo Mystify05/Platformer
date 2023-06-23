7d320ba7b3fdcd6175faec5f03c41eb9a044a98c
Arvin <arvin.vach@gmail.com>
2023-06-23 21:32:43 +0200

    updating files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f66d12d0c24ffd5bfe9be69186c47d2fa1d442e790b23f6d4f1b0f6e83cbb292
+size 4267
---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:115625f359650eee84d52d5855feedb26677be837c1d8756fb036b743c81dde7
+size 1218
